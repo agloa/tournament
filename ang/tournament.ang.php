0211848525b15e192e98f6b60c1b196198fdeafb
Mike Steigerwald <CIO@AGLOA.org>
2018-12-12 14:35:51 -0800

    Refactor Person and Address like https://docs.angularjs.org/tutorial

--- ang/tournament.ang.php
+++ ang/tournament.ang.php
@@ -1,22 +1,24 @@
 <?php
-// This file declares an Angular module which can be autoloaded in CiviCRM. 
-// See also:
-//   http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+// This file declares an Angular module which can be autoloaded
+// in CiviCRM. See also:
+// http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
 
 return array (
   'js' => 
   array (
-    0 => 'ang/tournament.js',
+    0 => 'ang/tournament.module.js',
     1 => 'ang/tournament/*.js',
     2 => 'ang/tournament/*/*.js',
+    3 => 'ang/tournament/*/*.*.js',
   ),
   'css' => 
   array (
     0 => 'ang/tournament.css',
   ),
-  'partials' => // HTML?
+  'partials' => 
   array (
     0 => 'ang/tournament',
+    1 => 'ang/tournament/person',
   ),
   'settings' => 
   array (
